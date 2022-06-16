c940479e0d70f49d6e9685412cead87d27497ba3
amiguez <amiguez@student.42lyon.fr>
2022-06-16 08:57:44 +0200

    Need to make a fork Fork needed for semaphor ! i crying for help Maybe a rework will be needed

--- philo_bonus/includes/philo.h
+++ philo_bonus/includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: amiguez <amiguez@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/16 12:53:13 by amiguez           #+#    #+#             */
-/*   Updated: 2022/06/16 05:43:32 by amiguez          ###   ########.fr       */
+/*   Updated: 2022/06/16 08:35:30 by amiguez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,24 +17,24 @@
 # include <sys/time.h>
 # include <stdio.h>
 # include <pthread.h>
+# include <semaphore.h>
 
 # define DEAD 0
 # define ALIVE 1
 
 # define WRONG_ARGS 1
 # define WRONG_ARGS_2 2
-# define MALLOC_ERROR 3
-# define MALLOC_ERROR_2 4
-# define MUTEX_ERROR 5
-# define THREAD_ERROR 6
+# define ERROR_SEM 3
+# define ERROR_MALLOC 4
+# define ERROR_THREAD 5
+
+# define SEMAFORK "/sem_fork"
 
 typedef struct s_ph	t_ph;
 
 typedef struct s_lst_ph
 {
 	int				id;
-	int				fork_left;
-	int				fork_right;
 	t_ph			*data;
 	int				alive;
 	int				eat;
@@ -52,17 +52,17 @@ typedef struct s_ph
 	struct timeval	start;
 	struct timeval	current;
 	t_lst_ph		*lst_philo;
-	pthread_mutex_t	*mutex;
+	sem_t			*sem_fork;
 }		t_ph;
 
 //==================//
 //      main.c      //
 
 // int		main(int argc, char **argv);
+int			ft_launch(t_ph *data);
 int			ft_init(t_ph *data);
-int			ft_create_philo(t_ph *data);
 void		kill_all(t_ph *data);
-int			ft_exit(t_ph *data);
+void		ft_exit(t_ph *data);
 
 //      pars.c      //
 
@@ -71,22 +71,21 @@ int			ft_check_values(char **argv);
 
 //      error.c      //
 
-int			ft_error(int i, t_ph *data);
-int			ft_error_mutex(int i, t_ph *data);
-int			ft_error_thread(int i, t_ph *data);
+int			ft_error(int i, t_ph *data, int place);
+void		ft_error_thread(int place, t_ph *data);
 
-//      time.c      //
+//      time.c       //
 
 int			check_dead(t_ph *data);
 void		calc_last_eat(t_ph *data, int i);
 u_int64_t	get_time(t_ph *data);
 void		ft_usleep(u_int64_t time);
 
-//     thread.c      //
+//    simulation.c   //
 
-void		*ft_thread(void *arg);
-void		ft_eat(t_lst_ph *ph, t_ph *data);
-void		ft_sleep(t_lst_ph *ph, t_ph *data);
+void		*ft_philo(void *arg);
+void		ft_eat(t_ph *data, t_lst_ph *ph);
+void		ft_sleep(t_ph *data, t_lst_ph *ph);
 void		print_act(t_ph *data, int i, char *str, int pass);
 
 #endif
---
+++ philo_bonus/srcs/time.c
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   time.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amiguez <amiguez@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/10 19:55:58 by amiguez           #+#    #+#             */
+/*   Updated: 2022/06/16 03:26:22 by amiguez          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/philo.h"
+
+int	check_dead(t_ph *data)
+{
+	int				i;
+	struct timeval	now;
+	int				eat;
+
+	gettimeofday(&now, NULL);
+	i = 0;
+	eat = 1;
+	while (i < data->nb_philo)
+	{
+		if (data->lst_philo[i].eat != 0)
+			eat = 0;
+		calc_last_eat(data, i);
+		if (data->lst_philo[i].alive == DEAD)
+		{
+			print_act(data, i, "died\n", 1);
+			return (0);
+		}
+		i++;
+	}
+	if (eat == 1)
+		return (0);
+	return (1);
+}
+
+void	calc_last_eat(t_ph *data, int i)
+{
+	struct timeval	now;
+	int				time_diff;
+
+	gettimeofday(&now, NULL);
+	time_diff = ((now.tv_sec - data->lst_philo[i].last_eat.tv_sec) * 1000
+			+ (now.tv_usec - data->lst_philo[i].last_eat.tv_usec) / 1000);
+	if (time_diff > data->time_to_die)
+		data->lst_philo[i].alive = DEAD;
+}
+
+u_int64_t	get_time(t_ph *data)
+{
+	u_int64_t	start;
+	u_int64_t	current;
+
+	gettimeofday(&data->current, NULL);
+	start = (u_int64_t) data->start.tv_sec * 1000 + data->start.tv_usec / 1000;
+	current = (u_int64_t) data->current.tv_sec * 1000
+		+ data->current.tv_usec / 1000;
+	return (current - start);
+}
+
+void	ft_usleep(u_int64_t time)
+{
+	struct timeval	now;
+	struct timeval	current;
+
+	gettimeofday(&now, NULL);
+	current = now;
+	while (1)
+	{
+		usleep(10);
+		gettimeofday(&now, NULL);
+		if (((now.tv_sec - current.tv_sec) * 1000
+				+ (now.tv_usec - current.tv_usec) / 1000) > time)
+			break ;
+	}
+}
